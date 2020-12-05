75acf80f11be04a446f26348de996f9c5fa583a6
root <root@konstantinos>
2020-12-06 01:29:01 +0200

    Finished

--- PrivateSchoolSA/src/util/DbUtil.java
+++ PrivateSchoolSA/src/util/DbUtil.java
@@ -7,8 +7,7 @@ public class DbUtil {
 
     private static Connection connection = null;
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost/privateschool"+
-                    "?useSSL=false&serverTimeZone=UTC";
+    private static final String DB_URL = "jdbc:mysql://localhost/privateschool";
     private static final String USER = "root";
     private static final String PASS = "password";
 
